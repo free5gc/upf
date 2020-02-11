49836ceb819a5944f00f2a74240e6efc2599c5e6
張耀文 <hadeschang0322.cs04@g2.nctu.edu.tw>
2020-02-11 21:58:26 +0800

    Add get PDR/FAR function

--- lib/gtpv1/include/gtp_tunnel.h
+++ lib/gtpv1/include/gtp_tunnel.h
@@ -7,12 +7,12 @@
 
 Status GtpTunnelAddPdr(const char *ifname, struct gtp5g_pdr *pdr);
 Status GtpTunnelModPdr(const char *ifname, struct gtp5g_pdr *pdr);
-Status GtpTunnelDelPdr(const char *ifname, uint32_t id);
-Status GtpTunnelFindPdrById(const char *ifname, uint32_t id);
+Status GtpTunnelDelPdr(const char *ifname, uint16_t id);
+struct gtp5g_pdr *GtpTunnelFindPdrById(const char *ifname, uint16_t id);
 
 Status GtpTunnelAddFar(const char *ifname, struct gtp5g_far *far);
 Status GtpTunnelModFar(const char *ifname, struct gtp5g_far *far);
 Status GtpTunnelDelFar(const char *ifname, uint32_t id);
-Status GtpTunnelFindFarById(const char *ifname, uint32_t id);
+struct gtp5g_far *GtpTunnelFindFarById(const char *ifname, uint32_t id);
 
 #endif /* __GTP_TUNNEL_H__ */
--- lib/gtpv1/src/gtp_tunnel.c
+++ lib/gtpv1/src/gtp_tunnel.c
@@ -1,5 +1,7 @@
 #include "gtp_tunnel.h"
 
+#include "utlt_debug.h"
+
 #include <net/if.h>
 #include <libmnl/libmnl.h>
 #include <linux/genetlink.h>
@@ -12,50 +14,50 @@ Status GtpTunnelAddPdr(const char *ifname, struct gtp5g_pdr *pdr) {
     Status status;
     NetlinkInfo info;
 
+    UTLT_Assert(pdr, return STATUS_ERROR, "PDR is NULL");
+
     status = NetlinkSockOpen(&info, ifname, "gtp5g");
-    UTLT_Assert(status == STATUS_OK, goto err, "NetlinkSockOpen fail");
+    UTLT_Assert(status == STATUS_OK, return STATUS_ERROR, "NetlinkSockOpen fail");
 
     struct gtp5g_dev *dev = gtp5g_dev_alloc();
     gtp5g_dev_set_ifidx(dev, info.ifidx);
 
-    gtp5g_add_pdr(info.genl_id, info.nl, dev, pdr);
+    UTLT_Assert(!gtp5g_add_pdr(info.genl_id, info.nl, dev, pdr), status = STATUS_ERROR,
+        "GtpTunnelAddPdr Fail: PDR id[%u]", *gtp5g_pdr_get_id(pdr));
 
     gtp5g_dev_free(dev);
     NetlinkSockClose(&info);
 
-    return STATUS_OK;
-
-err:
-    return STATUS_ERROR;
+    return status;
 }
 
 Status GtpTunnelModPdr(const char *ifname, struct gtp5g_pdr *pdr) {
     Status status;
     NetlinkInfo info;
 
+    UTLT_Assert(pdr, return STATUS_ERROR, "PDR is NULL");
+
     status = NetlinkSockOpen(&info, ifname, "gtp5g");
-    UTLT_Assert(status == STATUS_OK, goto err, "NetlinkSockOpen fail");
+    UTLT_Assert(status == STATUS_OK, return STATUS_ERROR, "NetlinkSockOpen fail");
 
     struct gtp5g_dev *dev = gtp5g_dev_alloc();
     gtp5g_dev_set_ifidx(dev, info.ifidx);
 
-    gtp5g_mod_pdr(info.genl_id, info.nl, dev, pdr);
+    UTLT_Assert(!gtp5g_mod_pdr(info.genl_id, info.nl, dev, pdr), status = STATUS_ERROR,
+        "GtpTunnelModPdr Fail: PDR id[%u]", *gtp5g_pdr_get_id(pdr));
 
     gtp5g_dev_free(dev);
     NetlinkSockClose(&info);
 
-    return STATUS_OK;
-
-err:
-    return STATUS_ERROR;
+    return status;
 }
 
-Status GtpTunnelDelPdr(const char *ifname, uint32_t id) {
+Status GtpTunnelDelPdr(const char *ifname, uint16_t id) {
     Status status;
     NetlinkInfo info;
 
     status = NetlinkSockOpen(&info, ifname, "gtp5g");
-    UTLT_Assert(status == STATUS_OK, goto err, "NetlinkSockOpen fail");
+    UTLT_Assert(status == STATUS_OK, return STATUS_ERROR, "NetlinkSockOpen fail");
 
     struct gtp5g_dev *dev = gtp5g_dev_alloc();
     gtp5g_dev_set_ifidx(dev, info.ifidx);
@@ -63,24 +65,22 @@ Status GtpTunnelDelPdr(const char *ifname, uint32_t id) {
     struct gtp5g_pdr *pdr = gtp5g_pdr_alloc();
     gtp5g_pdr_set_id(pdr, id);
     
-    gtp5g_del_pdr(info.genl_id, info.nl, dev, pdr);
+    UTLT_Assert(!gtp5g_del_pdr(info.genl_id, info.nl, dev, pdr), status = STATUS_ERROR,
+        "GtpTunnelDelPdr fail: PDR id[%u]", id);
 
     gtp5g_pdr_free(pdr);
     gtp5g_dev_free(dev);
     NetlinkSockClose(&info);
 
-    return STATUS_OK;
-
-err:
-    return STATUS_ERROR;
+    return status;
 }
 
-Status GtpTunnelFindPdrById(const char *ifname, uint32_t id) {
+struct gtp5g_pdr *GtpTunnelFindPdrById(const char *ifname, uint16_t id) {
     Status status;
     NetlinkInfo info;
 
     status = NetlinkSockOpen(&info, ifname, "gtp5g");
-    UTLT_Assert(status == STATUS_OK, goto err, "NetlinkSockOpen fail");
+    UTLT_Assert(status == STATUS_OK, return NULL, "NetlinkSockOpen fail");
 
     struct gtp5g_dev *dev = gtp5g_dev_alloc();
     gtp5g_dev_set_ifidx(dev, info.ifidx);
@@ -88,58 +88,57 @@ Status GtpTunnelFindPdrById(const char *ifname, uint32_t id) {
     struct gtp5g_pdr *pdr = gtp5g_pdr_alloc();
     gtp5g_pdr_set_id(pdr, id);
     
-    gtp5g_pdr_find_by_id(info.genl_id, info.nl, dev, pdr);
+    struct gtp5g_pdr *rt_pdr;
+    UTLT_Assert((rt_pdr = gtp5g_pdr_find_by_id(info.genl_id, info.nl, dev, pdr)), , 
+        "GtpTunnelFindPdrById fail: PDR id[%u]", id);
 
     gtp5g_pdr_free(pdr);
     gtp5g_dev_free(dev);
     NetlinkSockClose(&info);
 
-    return STATUS_OK;
-
-err:
-    return STATUS_ERROR;
+    return rt_pdr;
 }
 
 Status GtpTunnelAddFar(const char *ifname, struct gtp5g_far *far) {
     Status status;
     NetlinkInfo info;
 
+    UTLT_Assert(far, return STATUS_ERROR, "FAR is NULL");
+
     status = NetlinkSockOpen(&info, ifname, "gtp5g");
-    UTLT_Assert(status == STATUS_OK, goto err, "NetlinkSockOpen fail");
+    UTLT_Assert(status == STATUS_OK, return STATUS_ERROR, "NetlinkSockOpen fail");
 
     struct gtp5g_dev *dev = gtp5g_dev_alloc();
     gtp5g_dev_set_ifidx(dev, info.ifidx);
 
-    gtp5g_add_far(info.genl_id, info.nl, dev, far);
+    UTLT_Assert(!gtp5g_add_far(info.genl_id, info.nl, dev, far), status = STATUS_ERROR,
+        "GtpTunnelAddFar fail: FAR id[%u]", *gtp5g_far_get_id(far));
 
     gtp5g_dev_free(dev);
     NetlinkSockClose(&info);
 
-    return STATUS_OK;
-
-err:
-    return STATUS_ERROR;
+    return status;
 }
 
 Status GtpTunnelModFar(const char *ifname, struct gtp5g_far *far) {
     Status status;
     NetlinkInfo info;
 
+    UTLT_Assert(far, return STATUS_ERROR, "FAR is NULL");
+
     status = NetlinkSockOpen(&info, ifname, "gtp5g");
-    UTLT_Assert(status == STATUS_OK, goto err, "NetlinkSockOpen fail");
+    UTLT_Assert(status == STATUS_OK, return STATUS_ERROR, "NetlinkSockOpen fail");
 
     struct gtp5g_dev *dev = gtp5g_dev_alloc();
     gtp5g_dev_set_ifidx(dev, info.ifidx);
 
-    gtp5g_mod_far(info.genl_id, info.nl, dev, far);
+    UTLT_Assert(!gtp5g_mod_far(info.genl_id, info.nl, dev, far), status = STATUS_ERROR,
+        "GtpTunnelModFar fail: FAR id[%u]", *gtp5g_far_get_id(far));
 
     gtp5g_dev_free(dev);
     NetlinkSockClose(&info);
 
-    return STATUS_OK;
-
-err:
-    return STATUS_ERROR;
+    return status;
 }
 
 Status GtpTunnelDelFar(const char *ifname, uint32_t id) {
@@ -147,7 +146,7 @@ Status GtpTunnelDelFar(const char *ifname, uint32_t id) {
     NetlinkInfo info;
 
     status = NetlinkSockOpen(&info, ifname, "gtp5g");
-    UTLT_Assert(status == STATUS_OK, goto err, "NetlinkSockOpen fail");
+    UTLT_Assert(status == STATUS_OK, return STATUS_ERROR, "NetlinkSockOpen fail");
 
     struct gtp5g_dev *dev = gtp5g_dev_alloc();
     gtp5g_dev_set_ifidx(dev, info.ifidx);
@@ -155,24 +154,22 @@ Status GtpTunnelDelFar(const char *ifname, uint32_t id) {
     struct gtp5g_far *far = gtp5g_far_alloc();
     gtp5g_far_set_id(far, id);
 
-    gtp5g_del_far(info.genl_id, info.nl, dev, far);
+    UTLT_Assert(!gtp5g_del_far(info.genl_id, info.nl, dev, far), status = STATUS_ERROR,
+        "GtpTunnelDelFar fail: FAR id[%u]", id);
 
     gtp5g_far_free(far);
     gtp5g_dev_free(dev);
     NetlinkSockClose(&info);
 
-    return STATUS_OK;
-
-err:
-    return STATUS_ERROR;
+    return status;
 }
 
-Status GtpTunnelFindFarById(const char *ifname, uint32_t id) {
-        Status status;
+struct gtp5g_far *GtpTunnelFindFarById(const char *ifname, uint32_t id) {
+    Status status;
     NetlinkInfo info;
 
     status = NetlinkSockOpen(&info, ifname, "gtp5g");
-    UTLT_Assert(status == STATUS_OK, goto err, "NetlinkSockOpen fail");
+    UTLT_Assert(status == STATUS_OK, return NULL, "NetlinkSockOpen fail");
 
     struct gtp5g_dev *dev = gtp5g_dev_alloc();
     gtp5g_dev_set_ifidx(dev, info.ifidx);
@@ -180,14 +177,13 @@ Status GtpTunnelFindFarById(const char *ifname, uint32_t id) {
     struct gtp5g_far *far = gtp5g_far_alloc();
     gtp5g_far_set_id(far, id);
 
-    gtp5g_far_find_by_id(info.genl_id, info.nl, dev, far);
+    struct gtp5g_far *rt_far;
+    UTLT_Assert((rt_far = gtp5g_far_find_by_id(info.genl_id, info.nl, dev, far)), ,
+        "GtpTunnelFindFarById fail: FAR id[%u]", id);
 
     gtp5g_far_free(far);
     gtp5g_dev_free(dev);
     NetlinkSockClose(&info);
 
-    return STATUS_OK;
-
-err:
-    return STATUS_ERROR;
+    return rt_far;
 }
